d70eeaada1570c8bc3dd6316854b7cbb14757125
Saad Hasan <saadh393@gmail.com>
2021-05-04 15:09:27 +0600

    Added Social Links

--- src/component/LeftOverlay/LeftOverlay.js
+++ src/component/LeftOverlay/LeftOverlay.js
@@ -5,28 +5,38 @@
 ⌚ Date : 22/ April/ 2021 
 */
 
-
 /*  🔥 React Dependencies 🔥 */
-import style from './LetOverlay.module.css'
-import facebook from '../../image/facebook.svg'
-import linkedin from '../../image/linkedin.svg'
-import github from '../../image/github.svg'
-
+import style from "./LetOverlay.module.css";
+import facebook from "../../image/facebook.svg";
+import linkedin from "../../image/linkedin.svg";
+import github from "../../image/github.svg";
 
 const LeftOverlay = () => {
-    return (
-        <>
-            <div className={style.LeftOverlay}>
-                <div className={style.line}></div>
-                <div className={style.socialIcons}>
-                     <object type="image/svg+xml" data={github} className={style.github}>Github</object>
-                     <object type="image/svg+xml" data={linkedin} className={style.linkedin}>Linkedin</object>
-                     <object type="image/svg+xml" data={facebook} className={style.facebook}>Facebook</object>
-                </div>
-                <div className={style.line}></div>
-            </div>
-        </>
-    );
+  const handleMedia = (sitename) => {
+    if (sitename === "github") {
+      window.open("https://github.com/saadh393", "_blank");
+    }
+    if (sitename === "linkedin") {
+      window.open("https://www.linkedin.com/in/saadh393/", "_blank");
+    }
+
+    if (sitename === "facebook") {
+      window.open("https://www.facebook.com/saadh393", "_blank");
+    }
+  };
+  return (
+    <>
+      <div className={style.LeftOverlay}>
+        <div className={style.line}></div>
+        <div className={style.socialIcons}>
+          <img src={github} className={style.github} onClick={() => handleMedia("github")} />
+          <img src={linkedin} className={style.linkedin} onClick={() => handleMedia("linkedin")} />
+          <img src={facebook} className={style.facebook} onClick={() => handleMedia("facebook")} />
+        </div>
+        <div className={style.line}></div>
+      </div>
+    </>
+  );
 };
 
-export default LeftOverlay
+export default LeftOverlay;
