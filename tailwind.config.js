5c3bf4a26c46f042b46a95195f1fc919381d90b3
Saad Hasan <saadh393@gmail.com>
2022-05-01 00:50:09 +0600

    Next topic is to my tools/stacks

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        background: "#121119",
+        "portfolio-blue": "#4F3DFE",
+        "portfolio-red": "#FF316F",
+        "portfolio-yellow": "#F9B813",
+        "portfolio-text": "#565966",
+      },
+    },
+  },
+  plugins: [],
+};
