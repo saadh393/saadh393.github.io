a9ef85dd94cd0bd7bc40d5b42ddc425bd0873258
saadh393 <saadh393@users.noreply.github.com>
2024-11-24 10:39:21 +0000

    deploy: a7ab7de82a8b16cd71f260a93132893ec9969e32

---
+++ _next/static/chunks/app/blogs/layout-02102f8a9b9082c5.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[339],{3177:function(n,_,e){Promise.resolve().then(e.t.bind(e,4766,23))},4766:function(n){n.exports={style:{fontFamily:"'__IaWriterFont_4f70a7', '__IaWriterFont_Fallback_4f70a7'"},className:"__className_4f70a7"}}},function(n){n.O(0,[971,69,744],function(){return n(n.s=3177)}),_N_E=n.O()}]);
